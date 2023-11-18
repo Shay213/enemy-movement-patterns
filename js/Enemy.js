8fbcf1e17edee533882f055136bd77ee0cab75a8
shay213 <dawidpawelec@o2.pl>
2023-11-18 15:19:22 +0100

    Draw sprites instead of rects

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,18 +1,31 @@
 export default class Enemy{
   constructor(canvasWidth, canvasHeight){
+    this.img = new Image()
+    this.img.src = '../assets/enemies/enemies/enemy1.png'
     this.x = Math.random() * canvasWidth
     this.y = Math.random() * canvasHeight
-    this.width = 50
-    this.height = 50
     this.speed = Math.random() * 4 - 2
+    this.spriteWidth = 293
+    this.spriteHeight = 155
+    this.width = this.spriteWidth / 2
+    this.height = this.spriteHeight / 2
+    this.frame = 0
+    this.gameFrame = 0
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1)
   }
 
   update(){
     this.x += this.speed
     this.y += this.speed
+    this.gameFrame++
+
+    // animate sprites
+    if(this.gameFrame % this.flapSpeed === 0){
+      this.frame > 4 ? this.frame = 0 : this.frame++
+    }
   }
 
   draw(ctx){
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.img, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import createEnemies from "./createEnemies.js"
+import Enemy from './Enemy.js'
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
@@ -6,11 +6,17 @@ const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 1000
 const CANVAS_HEIGHT = canvas.height = 700
 
-const enemies = createEnemies(20, CANVAS_WIDTH, CANVAS_HEIGHT)
+let numOfEnemies = 20
+
+const enemies = []
+
+for(let i = 0; i < numOfEnemies; i++){
+  enemies.push(new Enemy(CANVAS_WIDTH, CANVAS_HEIGHT))
+}
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  
+
   enemies.forEach(enemy => {
     enemy.update()
     enemy.draw(ctx)
